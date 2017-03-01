ffd71b1304543a6380725b68680e15d8a5824219
cicikus <goksenincakir@gmail.com>
2017-03-01 20:21:33 +0100

    update

--- HW3/1.1/passwrdcrack.py
+++ HW3/1.1/passwrdcrack.py
@@ -1,68 +1,53 @@
-import string
-
-ALLOWED_CHARACTERS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
-NUMBER_OF_CHARACTERS = len(ALLOWED_CHARACTERS)
-
-def characterToIndex(char):
-	return ALLOWED_CHARACTERS.index(char)
-
-def indexToCharacter(index):
-	if NUMBER_OF_CHARACTERS <= index:
-		raise ValueError("Index out of range.")
-	else:
-		return ALLOWED_CHARACTERS[index]
-
-def next(string):
-	if len(string) <= 0:
-		string.append(indexToCharacter(0))
-	else:
-		string[0] = indexToCharacter((characterToIndex(string[0]) + 1) % len(ALLOWED_CHARACTERS))
-		if characterToIndex(string[0]) is 0:
-			return list(string[0]) + next(string[1:])
-	return string
-
-def main():
-	sequence = list()
-	for x in range(1,100):
-		sequence = next(sequence)
-		print(sequence)
-	
-	print(ord("'"))
-	print(chr(92))
-
-if __name__ == "__main__":
-	main()
-
-"""
 import random
 
-def generate(n):
+def printAllKLengthRec(lst, prefix, n, k, container):
+	if k == 0:
+		# print(prefix)
+		container.append(prefix)
+		return
+	for i in range(0, n):
+		newprefix = prefix + lst[i]
+		printAllKLengthRec(lst, newprefix, n, k-1, container)
+
+def printAllKLength(lst, k, container):
+	n = len(lst)
+	printAllKLengthRec(lst, "", n, k, container)
+
+def generate_password(n):
 	passwd = ""
 	for i in range(0, n):
 		passwd += chr(random.randrange(33, 128))
 	return passwd
-	
-def crack(password):
-	combination = "!!!!!"
-	cracked_letter = 33
-	for i in range(len(combination)-1, 0, -1):
-		combination[i] = chr(ord(combination[i])+1)
-	# for char in cracked:
-	# 	letter_change = change
-	# 	ord(char)
-	# for char in password:
-	# 	while chr(cracked_letter) != char:
-	# 		cracked_letter += 1
-	# 	cracked += chr(cracked_letter)
-	# 	cracked_letter = 33
-	# return cracked
 
+def crack_password(password, container):
+	for element in container:
+		if element == password:
+			return element
 
+def main():
+	possible_characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+	l1 = list(possible_characters)
+	l2 = list()
+	printAllKLength(l1, 3, l2)
+	passwd = generate_password(3)
+	print(passwd)
+	print(crack_password("ABC", l2))
+	print(crack_password(passwd, l2))
 
+	# print("\nFirst test\n")
+	# l1 = list("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+	# l2 = list()
+	# k = 3;
+	# printAllKLength(l1, k, l2)
+	# #print(l2)
 
-passwd = generate(5)
-print(passwd)
-print(chr(77) == "M")
-print(crack(passwd))
-print(ord("M"))
-"""
+	# print("\nSecond test\n")
+	# l3 = list("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+	# l4 = list()
+	# k = 1
+	# printAllKLength(l3, k, l4)
+	# #print(l4)
+	# print("\n")
+
+if __name__ == "__main__":
+	main()
